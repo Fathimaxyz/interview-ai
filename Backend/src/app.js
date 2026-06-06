b349f60a8c9f809d4a610c4fb889a82098fe4e43
Fathima <fathimaah2005@gmail.com>
2026-06-06 14:38:45 +0530

    update cors for vercel frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-ai-theta-rose.vercel.app",
     credentials: true
 }))
 
